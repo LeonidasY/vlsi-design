1a478df64f908782b0c46fb570cb78f6a9a9f56f
LeonidasY <78405304+LeonidasY@users.noreply.github.com>
2021-07-31 17:53:21 +0800

    [Update] Added loop function to CP.

--- utils.py
+++ utils.py
@@ -12,25 +12,6 @@ def import_instances(folder):
             content = [x.strip() for x in content] 
             instances.append(content)
     return instances
-    
-# Method to output the solution
-# EX: output_solution('output/cp/out-0.txt', ['8', '4', '3 3', '3 5', '5 3', '5 5'], [5, 5, 0, 0], [5, 0, 5, 0], 8)
-def output_solution(file, instance, start_x, start_y, end_y):
-    solution = []
-    for i, x in enumerate(instance):
-        if i == 0:
-            solution.append(x + ' ' + str(end_y))
-        elif i == 1:
-            solution.append(x)
-        else:
-            solution.append(x + ' ' + str(start_x[i-2]) + ' ' + str(start_y[i-2]))
-    
-    print(solution)
-    
-    with open(file, 'w') as output:
-        for item in solution:
-            output.write(item)
-            output.write('\n')
 
 # Method used to plot the solution
 # EX: plot_solution(9, 12, 5, [[3, 3, 4, 0],[2, 4, 7, 0],[2, 8, 7, 4],[3, 9, 4, 3],[4, 12, 0, 0 ]])
@@ -52,3 +33,20 @@ def plot_solution(width, height, n_circuits, circuits):
     ax.set_yticks(range(height + 1))
     ax.grid(color='b', linewidth = 1)
     plt.show()
+    
+# Method to output the solution
+# EX: output_solution('output/cp/out-0.txt', ['8', '4', '3 3', '3 5', '5 3', '5 5'], [5, 5, 0, 0], [5, 0, 5, 0], 8)
+def output_solution(file, instance, start_x, start_y, end_y):
+    solution = []
+    for i, x in enumerate(instance):
+        if i == 0:
+            solution.append(x + ' ' + str(end_y))
+        elif i == 1:
+            solution.append(x)
+        else:
+            solution.append(x + ' ' + str(start_x[i-2]) + ' ' + str(start_y[i-2]))
+    
+    with open(file, 'w') as output:
+        for item in solution:
+            output.write(item)
+            output.write('\n')
